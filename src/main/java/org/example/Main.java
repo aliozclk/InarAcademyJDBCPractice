8876b9ab7f170a71ddf531d3e760ebb1292e0026
aliozclk <alliozcelik96@gmail.com>
2023-02-21 17:19:41 +0300

    Practices For JDBC

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+import java.sql.*;
+
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        System.out.println("Hello world!");
+
+        Question.answer();
+
+    }
+}
