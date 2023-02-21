b12ed42c63b00bc4f03c3b765729ac6a049661e3
aliozclk <alliozcelik96@gmail.com>
2023-02-21 17:19:30 +0300

    Practices For JDBC

---
+++ src/main/java/org/example/homework/ConnectionOfExercises.java
@@ -0,0 +1,32 @@
+package org.example.homework;
+
+import lombok.Data;
+
+import java.sql.*;
+
+@Data
+public class ConnectionOfExercises {
+    String dbURL = "jdbc:postgresql://localhost:5432/exercises";
+    String username="postgres";
+    String password = "ali.1996";
+
+    Connection connection;
+    Statement statement;
+
+    public void setConnection() {
+        try {
+            connection = connection = DriverManager.getConnection(dbURL, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setStatement() {
+
+        try {
+            statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
